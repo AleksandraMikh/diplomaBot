985367e4634f129c7d8db9103f7858dfd1a3896d
amikhailovskaia <102677574+AleksandraMikh@users.noreply.github.com>
2023-01-13 22:42:51 +0000

    1) Добавила прототип функции, которая формирует текст на диплом, вывожу её значения на картинку. Формировать текст будем по строкам. 2) Добавила в базу класс, пол, школу и город. Ещё сделала табличку для подписей старших по параллелям

--- db_watch.py
+++ db_watch.py
@@ -2,7 +2,8 @@ from replit import db
 
 
 def watch_the_base():
-  # del db["students"]
+  # for key in db.keys():
+  #   del db[key]
   print(db.keys())
   # print (db['statuses']['1'].keys())
   # for key in db.keys():
--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import time
 from typing import Tuple, Optional
 from PIL import Image, ImageDraw, ImageFont
 from io import BytesIO
-from telegram.ext import (CommandHandler, Updater, ConversationHandler,
-                          MessageHandler,
+from telegram.ext import (CommandHandler, Updater, MessageHandler,
                           Filters)  #upm package(python-telegram-bot)
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove  #upm package(python-telegram-bot)
+from telegram import (ReplyKeyboardMarkup,
+                      TelegramError)  # upm package(python-telegram-bot)
 from replit import db
 from db_script import fill_the_base
 import db_watch
@@ -19,31 +19,39 @@ updater = Updater(token=token)
 URL = 'https://api.thecatapi.com/v1/images/search'
 
 logging.basicConfig(
+  # filename='example.log',
+  # filemode='w',
   format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
   level=logging.DEBUG)
 
 
 def wake_up(update, context):
-  name = update.message.chat.first_name
-  reply_keyboard = [["/restart"]]
-  update.message.reply_text(
-    text=
-    f'Привет, {name}, я бот ТЛ2х2, я умею раздавать дипломы. Если вы хотите получить диплом за своего ребёнка, напишите его ФАМИЛИЮ, ИМЯ и ОТЧЕСТВО (при наличии).',
-    reply_markup=ReplyKeyboardMarkup(
-      reply_keyboard,
-      # one_time_keyboard=True,
-      resize_keyboard=True,
-      input_field_placeholder="ФИО ребенка"),
-  )
+    logging.info("Start wake_up funtion.")
+    name = update.message.chat.first_name
+    reply_keyboard = [["/restart"]]
+    update.message.reply_text(
+        text=(f'Привет, {name}, я бот ТЛ2х2, '
+              'я умею раздавать дипломы. Если вы '
+              'хотите получить диплом за своего ребёнка, '
+              'напишите его ФАМИЛИЮ, ИМЯ и ОТЧЕСТВО (при наличии).'),
+        reply_markup=ReplyKeyboardMarkup(
+            reply_keyboard,
+            # one_time_keyboard=True,
+            resize_keyboard=True,
+            input_field_placeholder="ФИО ребенка"),
+    )
+  
 
 
 def full_name_and_get_diploma(update, context):
   #TODO: переписать распаковку результата find_diploma
   #с учётом передачи текста для печати
+  logging.info("Start full_name_and_get_diploma funtion.")
   chat = update.effective_chat
   text_low = update.message.text.lower()
   words = text_low.split()
   if len(words) < 2:
+    logging.info(f"User {chat['username']} entered less than 2 words")
     context.bot.send_message(
       chat.id, (f"Мы не поняли ваше сообщение, вы написали только одно слово."
                 f" Введите фамилию, имя и отчество ребёнка через пробел."
@@ -51,12 +59,13 @@ def full_name_and_get_diploma(update, context):
     return
 
   (diploma_message, number_of_matches,
-   status) = find_diploma(last_name_or_whole_input=' '.join(words))
+   status,diploma_text) = find_diploma(last_name_or_whole_input=' '.join(words))
 
   if status:
     context.bot.send_message(text=diploma_message, chat_id=chat.id)
     byte_im = get_byte_image(last_name_or_whole_input=' '.join(words),
-                             status=status)
+                             status=status,
+                            diploma_text = diploma_text)
     update.message.reply_photo(photo=byte_im)
     context.bot.send_message(
       chat.id,
@@ -65,6 +74,7 @@ def full_name_and_get_diploma(update, context):
     return
 
   if number_of_matches > 1:
+    logging.info(f"User {chat['username']} got more than one match.")
     context.bot.send_message(text=diploma_message, chat_id=chat.id)
     return
 
@@ -78,13 +88,15 @@ def full_name_and_get_diploma(update, context):
     return
 
   if len(words) == 2:
+    logging.info("Start searching with swaped 2 words.")
     (diploma_message, number_of_matches,
-     status) = find_diploma(last_name_or_whole_input=words[1],
+     status,diploma_text) = find_diploma(last_name_or_whole_input=words[1],
                             first_name=words[0])
 
   if len(words) == 3:
+    logging.info("Start searching with swaped 3 words.")
     (diploma_message, number_of_matches,
-     status) = find_diploma(last_name_or_whole_input=words[2],
+     status,diploma_text) = find_diploma(last_name_or_whole_input=words[2],
                             first_name=words[0],
                             middle_name=words[1])
 
@@ -93,12 +105,14 @@ def full_name_and_get_diploma(update, context):
     if len(words) == 2:
       byte_im = get_byte_image(last_name_or_whole_input=words[1],
                                first_name=words[0],
-                               status=status)
+                               status=status,
+                              diploma_text = diploma_text)
     if len(words) == 3:
       byte_im = get_byte_image(last_name_or_whole_input=words[2],
                                first_name=words[0],
                                middle_name=words[1],
-                               status=status)
+                               status=status,
+                              diploma_text = diploma_text)
     update.message.reply_photo(photo=byte_im)
     context.bot.send_message(
       chat.id,
@@ -109,13 +123,23 @@ def full_name_and_get_diploma(update, context):
   context.bot.send_message(text=diploma_message, chat_id=chat.id)
 
 
-def get_text_for_diploma():
-  pass
+def get_text_for_diploma(full_name: str, 
+                        grade: str,
+                        gender: str,
+                        school:str,
+                        city: str,
+                        status: str):
+  return (full_name,
+         grade,
+         gender,
+         school,
+         city,
+         status)
 
 
 def find_diploma(last_name_or_whole_input: str,
                  first_name: str = '',
-                 middle_name: str = '') -> Tuple[str, int, Optional[str]]:
+                 middle_name: str = '') -> Tuple[str, int, Optional[str], Optional[str]]:
   '''Funcion searches for information about student in database.
     Middle name considered to be empty string if not provided.
     It returns tuple with message, number of matches and status of student if it's unique. 
@@ -126,12 +150,14 @@ def find_diploma(last_name_or_whole_input: str,
   #одно совпадение, использовать отдельную функцию (?)
   #возвращать текст в кортеже
   #TODO2: возвращать не кортеж, а словарь
+
   search_string = (f"{last_name_or_whole_input.lower()} "
                    f"{first_name.lower()} {middle_name.lower()}")
+                   
   logging.info(
-    f"Start searching for db record starts with {search_string.strip()}")
+    f"Start find_diploma function: search_string = {search_string.strip()}")
   matches = db.prefix(search_string.strip())
-  logging.info(f"{len(matches)} matches found")
+  logging.info(f"find_diploma function: {len(matches)} matches found")
   if len(matches) == 0:
     if 'ё' in search_string:
       new_search_string = search_string.replace('ё', 'е')
@@ -139,14 +165,14 @@ def find_diploma(last_name_or_whole_input: str,
       if res[1] == 0:
         new_message = res[
           0] + '\n \n А ещё я заметил, что в имени вашего ребёнка есть буква Ë. Я проверил, нет ли совпадений при замене всех Ë на Е, их не нашлось 😔'
-        res = (new_message, res[1], res[2])
+        res = (new_message, res[1], res[2], None)
       return res
     message = (f'Участник с именем {last_name_or_whole_input.title()}'
                f' {first_name.capitalize()} {middle_name.capitalize()}'
                f' не найден. Если вы ошиблись, отправьте ФИО ещё раз.'
                f' Если вы не ошиблись при печати имени,'
                f' свяжитесь с организатором по почте ?????@??.??')
-    return (message, len(matches), None)
+    return (message, len(matches), None, None)
   if len(matches) > 1:
     message = (f'Я нашёл {len(matches)} участников c именем'
                f' {last_name_or_whole_input.title()}'
@@ -154,7 +180,7 @@ def find_diploma(last_name_or_whole_input: str,
                f' Не знаю, что делать, но я что-нибудь придумаю.'
                f' А пока попробуйте ввести ФИО иначе - вдруг'
                f' получится найти именно вашего ребёнка.')
-    return (message, len(matches), None)
+    return (message, len(matches), None, None)
   if len(matches) == 1:
     message = (f'Участник с именем {last_name_or_whole_input.title()}'
                f' {first_name.capitalize()}'
@@ -163,13 +189,18 @@ def find_diploma(last_name_or_whole_input: str,
                f' Приступаем к формированию документа, это может занять'
                f' несколько минут.')
     #TODO: дописать здесь формирование текста для диплома
-    return (message, len(matches), db[matches[0]]["status"])
+    diploma_text = get_text_for_diploma(full_name = matches[0], 
+                                        **db[matches[0]]
+                                       )
+    return (message, len(matches), db[matches[0]]["status"], diploma_text)
 
 
 def get_byte_image(last_name_or_whole_input: str,
                    status: str,
+                   diploma_text: Tuple[str],
                    first_name: str = '',
                    middle_name: str = '') -> bytes:
+  logging.info("Start get_byte_image function.")
   imgstr = db["statuses"][status]["base_picture"]
   image = Image.open(BytesIO(base64.b64decode(imgstr)))
   add_text = ImageDraw.Draw(image)
@@ -178,17 +209,30 @@ def get_byte_image(last_name_or_whole_input: str,
          f" {first_name.capitalize()} {middle_name.capitalize()}!")
   # w, h = add_text.textsize(msg)
   # box = (100, 100, 500, 150)
-
+  #TODO: написать здесь рисование правильного текста
   add_text.text(
     (100, 100),
     msg,
     fill=('#000000'),
     font=font,
   )
+  i = 0
+  for text in diploma_text:
+    add_text.text(
+      (100, 120 + 20*i),
+      text,
+      fill=('#000000'),
+      font=font,
+    )
+    i+=1
   byteIO = BytesIO()
   image.save(byteIO, format='PNG')
   byteArr = byteIO.getvalue()
   return byteArr
+
+
+def error_handler(update, context):
+  logging.error(context.error)
 
 
 def main():
@@ -199,6 +243,7 @@ def main():
 
   updater.dispatcher.add_handler(start_handler)
   updater.dispatcher.add_handler(full_name_handler)
+  updater.dispatcher.add_error_handler(error_handler)
 
   updater.start_polling()
   updater.idle()
